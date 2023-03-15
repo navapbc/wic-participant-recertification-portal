1811e43595c858b81fab9b9e8676cd2ad22f5d1c
Rocket ! <rocketnova@users.noreply.github.com>
2023-03-14 22:25:39 -0700

    Bring over WIP to allow for per-app CD.

--- bin/deploy-release.sh
+++ bin/deploy-release.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 APP_NAME=$1
 IMAGE_TAG=$2
 ENV_NAME=$3
+INFRA_APP_NAME=$4
 
 # Need to init module when running in CD since GitHub actions does a fresh checkout of repo
-terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME init
-ACCOUNT_ID=$(terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME output -raw account_id)
+terraform -chdir=infra/$INFRA_APP_NAME/envs/$ENV_NAME init
+ACCOUNT_ID=$(terraform -chdir=infra/$INFRA_APP_NAME/envs/$ENV_NAME output -raw account_id)
 
 echo "--------------------------"
 echo "Deploy release parameters"
@@ -16,7 +17,8 @@ echo "APP_NAME=$APP_NAME"
 echo "IMAGE_TAG=$IMAGE_TAG"
 echo "ENV_NAME=$ENV_NAME"
 echo "ACCOUNT_ID=$ACCOUNT_ID"
+echo "INFRA_APP_NAME=$INFRA_APP_NAME"
 echo
 echo "Starting $APP_NAME deploy of $IMAGE_TAG to $ENV_NAME"
-terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME apply -auto-approve -var="image_tag=$IMAGE_TAG"
+terraform -chdir=infra/$INFRA_APP_NAME/envs/$ENV_NAME apply -auto-approve -var="image_tag=$IMAGE_TAG"
 echo "Completed $APP_NAME deploy of $IMAGE_TAG to $ENV_NAME"
