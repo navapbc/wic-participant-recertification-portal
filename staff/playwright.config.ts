d32a13c2c8614ec8687f5699e091eb36f5effe4c
Rocket ! <rocketnova@users.noreply.github.com>
2023-03-24 12:20:01 -0700

    Write bash script for running e2e docker commands. Update for staff portal.

--- staff/playwright.config.ts
+++ staff/playwright.config.ts
@@ -37,7 +37,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: "http://lowdefy-e2e:3000/",
+    baseURL: "http://app-e2e:3000/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
