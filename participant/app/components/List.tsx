0aca596798a880541a5c73f23452e1f77bbe8fb2
Rocket ! <rocketnova@users.noreply.github.com>
2023-03-23 18:32:54 -0700

    Create a List component and replace content lists with it.

---
+++ participant/app/components/List.tsx
@@ -0,0 +1,31 @@
+import type { ReactElement } from "react";
+import type { i18nKey, legendStyleType } from "app/types";
+import { Trans } from "react-i18next";
+
+export type ListProps = {
+  listKeys: i18nKey[];
+  ordered?: boolean;
+  unstyled?: boolean;
+};
+
+export const List = (props: NameInputProps): ReactElement => {
+  const {
+    listKeys,
+    ordered = false,
+    unstyled = false,
+  } = props;
+  const classNames = `usa-list ${ unstyled ? 'usa-list--unstyled' : ''}`
+  const ListTag = ordered ? "ol" : "ul";
+
+  return (
+    <ListTag className={classNames.trim()}>
+      {listKeys.map((key: string) => (
+        <li key={key}>
+          <Trans i18nKey={key} />
+        </li>
+      ))}
+    </ListTag>
+  );
+};
+
+export default List;
