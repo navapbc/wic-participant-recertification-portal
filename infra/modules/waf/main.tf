b15c7c392bc302337df96b7b527b4518e31b4e8b
Alsia Plybeah <alsia@navapbc.com>
2023-03-29 13:01:26 -0400

    update WAF ruleset to be more comprehensive

--- infra/modules/waf/main.tf
+++ infra/modules/waf/main.tf
@@ -9,7 +9,7 @@ resource "aws_wafv2_web_acl" "waf" {
   }
 
   rule {
-    name     = "BasicRules"
+    name     = "AWSGeneralRules"
     priority = 1
 
     override_action {
@@ -20,28 +20,6 @@ resource "aws_wafv2_web_acl" "waf" {
       managed_rule_group_statement {
         name        = "AWSManagedRulesCommonRuleSet"
         vendor_name = "AWS"
-
-        rule_action_override {
-          action_to_use {
-            count {}
-          }
-
-          name = "SizeRestrictions_QUERYSTRING"
-        }
-
-        rule_action_override {
-          action_to_use {
-            count {}
-          }
-
-          name = "NoUserAgent_HEADER"
-        }
-
-        scope_down_statement {
-          geo_match_statement {
-            country_codes = ["US"]
-          }
-        }
       }
     }
 
@@ -53,7 +31,7 @@ resource "aws_wafv2_web_acl" "waf" {
   }
 
   rule {
-    name     = "ManageKnownBadInputs"
+    name     = "AWSManageKnownBadInputs"
     priority = 1
     # setting to none re this solution here: https://github.com/bridgecrewio/checkov/issues/2101
     # count rule override: https://docs.aws.amazon.com/waf/latest/developerguide/web-acl-rule-group-override-options.html#web-acl-rule-group-override-options-rule-group
@@ -74,10 +52,162 @@ resource "aws_wafv2_web_acl" "waf" {
     }
   }
 
+  rule {
+    name        = "AWSIPReputationList"
+    description = "Inspect IPs that have been identified as bots by Amazon"
+    priority    = 1
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "ip-vulnerability-metrics"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name        = "AWSAnonList"
+    description = "Inspects IPs for services known to anonymize client information e.g. proxies"
+    override_action { # does this need an override?
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "ip-anon-metrics"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name        = "AWSSQLManagement"
+    description = " Blocks requests associated with SQL database exploitation"
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "sql-vulnerability-metrics"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name        = "AWSLinuxManagement"
+    description = " Blocks requests associated with Linux exploitation"
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "linux-vulnerability-metrics"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name        = "AWSUnixManagement"
+    description = " Blocks requests associated with POSIX and POSIX-like OS exploitation"
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesUnixRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "unix-vulnerability-metrics"
+      sampled_requests_enabled   = false
+    }
+  }
   visibility_config {
     cloudwatch_metrics_enabled = false
     metric_name                = "waf-general-metrics"
     sampled_requests_enabled   = false
+  }
+  # the following are custom rules and arent managed by AWS.
+  rule {
+    name     = "AWSRateBasedRuleDomesticDOS"
+    description = "Applies a rate based rule to IPs originating in the US"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 2000 # max number of requests per 5-minute period from a single originating IP address
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US"]
+          }
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "domestic-dos-metrics"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSRateBasedRuleGlobalDOS"
+    description = "Applies a rate based rule to IPs originating outside of the US"
+    priority = 2
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 500 # max number of requests per 5-minute period from a single originating IP address
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          not_statement {
+            statement {
+              geo_match_statement {
+                country_codes = ["US"]
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "global-dos-metrics"
+      sampled_requests_enabled   = true
+    }
   }
 }
 
