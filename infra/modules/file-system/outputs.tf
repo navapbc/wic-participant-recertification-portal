3c143e89d12caa73b0c065408283cf7fba099c4f
Rocket ! <rocketnova@users.noreply.github.com>
2023-03-29 15:11:20 -0700

    Cleanup commented out code.

--- infra/modules/file-system/outputs.tf
+++ infra/modules/file-system/outputs.tf
@@ -9,20 +9,3 @@ output "file_system" {
 output "access_point" {
   value = aws_efs_access_point.fs
 }
-
-
-# output "file_system_id" {
-#   value = aws_efs_file_system.fs.id
-# }
-
-# output "file_system_arn" {
-#   value = aws_efs_file_system.fs.arn
-# }
-
-# output "access_point_id" {
-#   value = aws_efs_access_point.fs.id
-# }
-
-# output "access_point_arn" {
-#   value = aws_efs_access_point.fs.arn
-# }
