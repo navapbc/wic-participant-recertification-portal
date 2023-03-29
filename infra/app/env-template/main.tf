7d11065a79967f22e8d753d2edc5365c46f8a264
Rocket ! <rocketnova@users.noreply.github.com>
2023-03-29 10:26:08 -0700

    Try deploying to AWS using our non-privileged port.

--- infra/app/env-template/main.tf
+++ infra/app/env-template/main.tf
@@ -129,16 +129,21 @@ module "analytics_database" {
 }
 
 module "analytics" {
-  source               = "../../modules/service"
-  service_name         = local.analytics_service_name
-  image_repository_url = "docker.io/library/matomo"
+  source       = "../../modules/service"
+  service_name = local.analytics_service_name
+  # image_repository_url = "docker.io/library/matomo"
+  image_repository_url = data.aws_ecr_repository.analytics_image_repository.repository_url
   image_repository_arn = data.aws_ecr_repository.analytics_image_repository.arn
-  image_tag            = "4.14.1"
-  vpc_id               = data.aws_vpc.default.id
-  subnet_ids           = data.aws_subnets.default.ids
-  service_cluster_arn  = module.service_cluster.service_cluster_arn
-  container_port       = 80
-  container_read_only  = false
+  # image_tag            = "4.14.1"
+  image_tag           = var.image_tag
+  vpc_id              = data.aws_vpc.default.id
+  subnet_ids          = data.aws_subnets.default.ids
+  service_cluster_arn = module.service_cluster.service_cluster_arn
+  # container_port       = 80
+  container_port      = 8080
+  container_read_only = false
+  enable_healthcheck  = false
+  healthcheck_path    = "/"
   container_secrets = [
     {
       name      = "MATOMO_DATABASE_HOST",
