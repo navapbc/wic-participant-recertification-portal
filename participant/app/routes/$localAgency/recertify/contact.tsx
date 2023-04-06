e6a082bc8fa4cc171f13baaccb13d0e16ccd6ae1
Makaela <makaela@navapbc.com>
2023-04-06 18:17:48 -0400

    format

--- participant/app/routes/$localAgency/recertify/contact.tsx
+++ participant/app/routes/$localAgency/recertify/contact.tsx
@@ -10,8 +10,8 @@ import { RequiredQuestionStatement } from "~/components/RequiredQuestionStatemen
 //import { withZod } from "@remix-validated-form/with-zod";
 
 export default function Contact() {
-    const { t } = useTranslation();
-    /**  const phoneNumberProps: TextFieldProps = {
+  const { t } = useTranslation();
+  /**  const phoneNumberProps: TextFieldProps = {
          id: "phoneNumberSize",
          type: "input",
          inputType: "tel",
@@ -27,38 +27,38 @@ export default function Contact() {
          required: false,
          labelClassName: "label-large",
      }; */
-    return (
-        <div>
-            <h1>
-                <Trans i18nKey="Contact.title" />
-            </h1>
-            <RequiredQuestionStatement />
-            <Form>
-                <TextField
-                    id="phone-number"
-                    inputType="tel"
-                    labelKey="Contact.phoneNumber"
-                    required
-                    type="input"
-                    labelClassName="usa-label--large"
-                />
-                <TextField
-                    id="additional-ifno"
-                    inputType="text"
-                    labelKey="Contact.additionalInfo.label"
-                    labelClassName="usa-label--large"
-                    hint={<Trans i18nKey="Contact.additionalInfo.hint" />}
-                    type="textarea"
-                />
+  return (
+    <div>
+      <h1>
+        <Trans i18nKey="Contact.title" />
+      </h1>
+      <RequiredQuestionStatement />
+      <Form>
+        <TextField
+          id="phone-number"
+          inputType="tel"
+          labelKey="Contact.phoneNumber"
+          required
+          type="input"
+          labelClassName="usa-label--large"
+        />
+        <TextField
+          id="additional-ifno"
+          inputType="text"
+          labelKey="Contact.additionalInfo.label"
+          labelClassName="usa-label--large"
+          hint={<Trans i18nKey="Contact.additionalInfo.hint" />}
+          type="textarea"
+        />
 
-                <Button
-                    className="display-block margin-top-6"
-                    type="submit"
-                    formMethod="post"
-                >
-                    <Trans i18nKey="Contact.button" />
-                </Button>
-            </Form>
-        </div>
-    );
+        <Button
+          className="display-block margin-top-6"
+          type="submit"
+          formMethod="post"
+        >
+          <Trans i18nKey="Contact.button" />
+        </Button>
+      </Form>
+    </div>
+  );
 }
