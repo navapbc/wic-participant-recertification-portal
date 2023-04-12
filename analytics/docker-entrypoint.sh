e4789a89e4f74fec30259557918463ca6372a75c
Rocket ! <rocketnova@users.noreply.github.com>
2023-04-12 00:27:40 -0700

    Still debugging.

--- analytics/docker-entrypoint.sh
+++ analytics/docker-entrypoint.sh
@@ -5,9 +5,11 @@ set -e
 ls -la
 ls -la /usr/src/matomo
 
-if [ ! -e matomo.php ]; then
-	tar cf - --one-file-system --owner=33 --group=33 -C /usr/src/matomo . | tar xf -
-fi
+rm -rf *
+# if [ ! -e matomo.php ]; then
+# 	tar cf - --one-file-system -C /usr/src/matomo . | tar xf -
+# fi
+tar cf - --one-file-system --owner=33 --group=33 -C /usr/src/matomo . | tar xf -
 
 ls -la /var/www/html
 
