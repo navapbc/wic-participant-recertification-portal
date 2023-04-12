a9afe2a104def346f58aa8767739e4d683192229
Rocket ! <rocketnova@users.noreply.github.com>
2023-04-12 00:44:36 -0700

    Still debugging entrypoint.

--- analytics/docker-entrypoint.sh
+++ analytics/docker-entrypoint.sh
@@ -2,15 +2,8 @@
 # Overwrites the matomo entrypoint.
 set -e
 
-ls -la
-ls -la /usr/src/matomo
-
-rm -rf *
-# if [ ! -e matomo.php ]; then
-# 	tar cf - --one-file-system -C /usr/src/matomo . | tar xf -
-# fi
-tar cf - --one-file-system --owner=33 --group=33 -C /usr/src/matomo . | tar xf -
-
-ls -la /var/www/html
+if [ ! -e matomo.php ]; then
+	tar cf - --one-file-system --owner=33 --group=33 -C /usr/src/matomo . | tar xf -
+fi
 
 exec "$@"
