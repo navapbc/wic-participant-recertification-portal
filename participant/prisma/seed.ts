fcd4c844a5516a16a0f3c5b807b100dd54046796
Rocket ! <rocketnova@users.noreply.github.com>
2023-04-14 00:01:07 -0700

    Tidy seed.

--- participant/prisma/seed.ts
+++ participant/prisma/seed.ts
@@ -3,18 +3,9 @@ import {
   findLocalAgency,
   upsertLocalAgency,
 } from "app/utils/db.server";
+import agencyData from "public/data/local-agencies.json"
 
 const prisma = new PrismaClient();
-const agencyData = [
-  {
-    urlId: "gallatin",
-    name: "Gallatin",
-  },
-  {
-    urlId: "missoula",
-    name: "Missoula",
-  },
-];
 // Any interactions with Prisma will be async
 // eslint-disable-next-line @typescript-eslint/require-await
 async function seed() {
@@ -22,26 +13,13 @@ async function seed() {
   // You can access relations as normal here
   // const users = await prisma.user.findMany(); // (for example)
 
-  await agencyData.forEach(async singleAgencyData => {
-    const localAgency = await findLocalAgency(singleAgencyData.urlId);
-    if (!localAgency || localAgency.name != singleAgencyData.name) {
-      await upsertLocalAgency(singleAgencyData.urlId, singleAgencyData.name)
-      console.log(`Seeding localAgency: ${singleAgencyData.urlId} 🌱`)
+  for (const agency of agencyData) {
+    const localAgency = await findLocalAgency(agency.urlId);
+    if (!localAgency || localAgency.name != agency.name) {
+      await upsertLocalAgency(agency.urlId, agency.name)
+      console.log(`Seeding localAgency: ${agency.urlId} 🌱`)
     }
-  });
-
-  // const existingAgencyRecords = await prisma.localAgency.count();
-  // if (existingAgencyRecords != agencyData.length) {
-  //   await prisma.localAgency.createMany({
-  //     data: agencyData,
-  //     skipDuplicates: true,
-  //   });
-  //   console.log(`Database has been seeded. 🌱`);
-  // } else {
-  //   console.log(
-  //     `Skipping localAgency seed; found ${existingAgencyRecords} agencies 🪴`
-  //   );
-  // }
+  }
 }
 
 seed()
