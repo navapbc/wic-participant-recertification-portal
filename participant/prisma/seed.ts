5d05c387c2574310a4200ec9bb9af8601a1e29aa
Rocket ! <rocketnova@users.noreply.github.com>
2023-04-13 23:56:37 -0700

    Revert prisma versions.

--- participant/prisma/seed.ts
+++ participant/prisma/seed.ts
@@ -1,28 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import {
-  findSubmission,
-  upsertSubmission,
-  upsertSubmissionForm,
   findLocalAgency,
-  firstLocalAgency,
   upsertLocalAgency,
 } from "app/utils/db.server";
-import agencyData from "public/data/local-agencies.json";
-// import submissionsData from "public/data/submissions.not-prod.json";
-import segfaultHandler from "node-segfault-handler";
-
 
 const prisma = new PrismaClient();
-
+const agencyData = [
+  {
+    urlId: "gallatin",
+    name: "Gallatin",
+  },
+  {
+    urlId: "missoula",
+    name: "Missoula",
+  },
+];
 // Any interactions with Prisma will be async
 // eslint-disable-next-line @typescript-eslint/require-await
 async function seed() {
-  segfaultHandler.registerHandler();
-  // Put the actions you need to take to seed the database here.
+  // Put the actions you need to take to seed the databse here.
   // You can access relations as normal here
   // const users = await prisma.user.findMany(); // (for example)
 
-  // Seed local agencies.
   await agencyData.forEach(async singleAgencyData => {
     const localAgency = await findLocalAgency(singleAgencyData.urlId);
     if (!localAgency || localAgency.name != singleAgencyData.name) {
@@ -31,27 +30,18 @@ async function seed() {
     }
   });
 
-  // Seed submissions.
-  // Create development submission seed records.
-  // Object.entries(submissionsData).map(async ([singleAgencyId, singleAgencySubmissions]) => {
-  //   const localAgency = await findLocalAgency(singleAgencyId);
-  //   if (localAgency) {
-  //     singleAgencySubmissions.forEach(async submission => {
-  //       await upsertSubmission(submission.submissionId, localAgency.urlId);
-  //       console.log("upserted submission")
-  //       // for (let [formRoute, formData] of Object.entries(submission.forms)) {
-  //       //   console.log("trying to upsert submission form")
-  //       //   await upsertSubmissionForm(
-  //       //     submission.submissionId,
-  //       //     formRoute,
-  //       //     formData
-  //       //   );
-  //       //   console.log("upserted submission form")
-  //       // }
-  //       console.log(`Seeding submission: ${submission.forms.name.firstName} ${submission.forms.name.lastName} 🌱`)
-  //     });
-  //   }
-  // });
+  // const existingAgencyRecords = await prisma.localAgency.count();
+  // if (existingAgencyRecords != agencyData.length) {
+  //   await prisma.localAgency.createMany({
+  //     data: agencyData,
+  //     skipDuplicates: true,
+  //   });
+  //   console.log(`Database has been seeded. 🌱`);
+  // } else {
+  //   console.log(
+  //     `Skipping localAgency seed; found ${existingAgencyRecords} agencies 🪴`
+  //   );
+  // }
 }
 
 seed()
