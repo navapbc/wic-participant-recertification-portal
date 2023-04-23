a2cbe4c8764edfcb703bc93a191c8cdb481307e0
Rocket ! <rocketnova@users.noreply.github.com>
2023-04-22 21:21:21 -0700

    Tidy.

--- bin/create-staff-users.sh
+++ bin/create-staff-users.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
+# A script to create users in AWS Cognito and create database records for them in the participant database.
+# Run from the root of the project like so: ./bin/create-staff-users.sh <env name>
 set -euo pipefail
 
+# Positional arguments
 ENV_NAME=$1
-# PROJECT_NAME=$1
-# APP_NAME=$2
-# IMAGE_TAG=$4
-# INFRA_APP_NAME=$5
+
+# Variables
+INPUT_JSON_FILENAME="staff-uuids-to-agencies.json"
+USER_POOL_NAME="user_pool_name=wic-prp-staff-${ENV_NAME}"
+CLUSTER_NAME="wic-prp-app-${ENV_NAME}"
+TASK_DEFINITION_NAME="wic-prp-participant-${ENV_NAME}"
+BUCKET_NAME="wic-prp-side-load-${ENV_NAME}"
 
 # Requires a file named `staff-emails-to-agencies.tfvars.json` in the infra/modules/cognito-staff-user dir.
 # See infra/modules/cognito-staff-user/staff-emails-to-agencies.tfvars.json.example for an example.
 
 # Create staff users in cognito.
 terraform -chdir=infra/modules/cognito-staff-user init
-terraform -chdir=infra/modules/cognito-staff-user apply -var-file=staff-emails-to-agencies.tfvars.json -var="user_pool_name=wic-prp-staff-${ENV_NAME}"
+terraform -chdir=infra/modules/cognito-staff-user apply \
+  -var-file=staff-emails-to-agencies.tfvars.json \
+  -var=$USER_POOL_NAME
 
 # Save output to s3.
-terraform -chdir=infra/modules/cognito-staff-user output -json | jq .staff_user_list.value > staff-uuids-to-agencies.json
+terraform -chdir=infra/modules/cognito-staff-user output -json | jq .staff_user_list.value > $INPUT_JSON_FILENAME
 aws s3api put-object \
-  --bucket "wic-prp-side-load-${ENV_NAME}" \
-  --key "seed/staff-uuids-to-agencies.json" \
-  --body staff-uuids-to-agencies.json
+  --bucket $BUCKET_NAME \
+  --key "seed/${INPUT_JSON_FILENAME}" \
+  --body $INPUT_JSON_FILENAME
 
 # Import staff users into participant database.
-# aws ecs run-task \
-#   --cluster "wic-prp-app-${ENV_NAME}" \
-#   --task-definition "wic-prp-participant-${ENV_NAME}" \
-#   --overrides '{ "containerOverrides": [{ "command": "npm run seed-staff-users" }, { "environment" : [{ "S3_BUCKET": "wic-prp-side-load-${ENV_NAME}" }]}]}'
+# Run as a one-off ECS task.
+aws ecs run-task \
+  --cluster $CLUSTER_NAME \
+  --task-definition $TASK_DEFINITION_NAME \
+  --overrides "{ \"containerOverrides\": [{ \"command\": \"npm run seed-staff-users\" }, { \"environment\" : [{ \"S3_BUCKET\": \"${BUCKET_NAME}\" }]}]}"
 
 # Cleanup
-rm staff-uuids-to-agencies.json
+rm $INPUT_JSON_FILENAME
