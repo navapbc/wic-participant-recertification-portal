b912681ae9d2bfbe1b94f54a0b7249a430fdfaa6
Rocket ! <rocketnova@users.noreply.github.com>
2023-04-24 01:26:32 -0700

    Accept easier to create file.

--- infra/modules/cognito-staff-user/create-staff-users.sh
+++ infra/modules/cognito-staff-user/create-staff-users.sh
@@ -1,33 +1,68 @@
 #!/bin/bash
-# A script to create users in AWS Cognito and create database records for them in the participant database.
+# This script creates users in AWS Cognito and create database records for them in the participant database.
+# It requires 2 positional arguments:
+# - the environment to create the users in
+# - the path to a json file that maps local agencies and emails (which are used as the username).
+# See below for more details.
+# Example: ./create-staff-users dev /tmp/email.json
 set -euo pipefail
 
 # Positional arguments.
+# The environment for the users to be created in.
 ENV_NAME=$1
 
+# The Cognito process needs a file with the emails for each local agency. This must be provided.
+# Do not commit this file to git! Pass in the path to a file of the format:
+# {
+#   "<local_agency_url_id>": [
+#     "email",
+#     "email",
+#     "email"
+#   ]
+# }
+#
+# For example:
+# {
+#   "gallatin": [
+#     "apple@wic.gov",
+#     "carrot@wic.gov"
+#     "eggplant@wic.gov"
+#   ],
+#   "missoula": [
+#     "banana@wic.gov",
+#     "daikonwic.gov",
+#   ]
+# }
+EMAIL_FILENAME=$2
+
 # Variables.
-# The Cognito process needs a file that maps emails to local agencies. This must be provided.
-# ⚠️ Warning! Don't commit this file to the git repo!
-EMAIL_FILENAME="staff-emails-to-agencies.tfvars.json"
-# The ECS Task process needs a file that maps Cognito UUIDs to local agencies. This is automatically created.
+# The emails get transformed into this format for terraform to ingest:
+# {
+#   "apple@wic.gov": "gallatin",
+#   "carrot@wic.gov": "gallatin",
+#   "eggplant@wic.gov": "gallatin",
+#   "banana@wic.gov": "missoula",
+#   "daikon@wic.gov": "missoula"
+# }
+TRANSFORMED_USER_EMAILS=$(jq '[to_entries[] | {(.value[]): .key}] | add' $EMAIL_FILENAME)
 UUID_FILENAME="staff-uuids-to-agencies.json"
-USER_POOL_NAME="user_pool_name=wic-prp-staff-${ENV_NAME}"
+USER_POOL_NAME="wic-prp-staff-${ENV_NAME}"
 CLUSTER_NAME="wic-prp-app-${ENV_NAME}"
 TASK_DEFINITION_NAME="wic-prp-participant-${ENV_NAME}"
 CONTAINER_NAME="wic-prp-participant-${ENV_NAME}"
 SERVICE_NAME="wic-prp-participant-${ENV_NAME}"
 BUCKET_NAME="wic-prp-side-load-${ENV_NAME}"
 
-# Requires a file named `staff-emails-to-agencies.tfvars.json` in the infra/modules/cognito-staff-user dir.
-# See infra/modules/cognito-staff-user/staff-emails-to-agencies.tfvars.json.example for an example.
 
-# Create staff users in cognito.
+# # Create staff users in cognito.
 terraform init
 terraform apply \
-  -var-file=$EMAIL_FILENAME \
-  -var=$USER_POOL_NAME
+  -var="user_pool_name=${USER_POOL_NAME}" \
+  -var="user_emails=${TRANSFORMED_USER_EMAILS}"
 
 # Save output to s3.
+# The ECS Task process needs a file that maps Cognito UUIDs to local agencies. This is automatically created and
+# temporarily saved locally. It will be cleaned up as the last step.
 terraform output -json | jq .staff_user_list.value > $UUID_FILENAME
 aws s3api put-object \
   --bucket $BUCKET_NAME \
@@ -40,7 +75,7 @@ CONTAINER_OVERRIDES="{ \"containerOverrides\": [{ \"name\": \"${CONTAINER_NAME}\
 
 # Import staff users into participant database.
 # Run as a one-off ECS task.
-# Debug command:
+# Logs can be viewed in Cloudwatch.
 aws ecs run-task \
   --cluster $CLUSTER_NAME \
   --task-definition $TASK_DEFINITION_NAME \
