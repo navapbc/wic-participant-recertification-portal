d92c24f20b0a26990fdb21f14eb4e89d8495cf71
Rocket ! <rocketnova@users.noreply.github.com>
2023-05-03 23:03:27 -0700

    Add scale in and scale out. Ignore checkov warning.

--- infra/modules/service-autoscaling/main.tf
+++ infra/modules/service-autoscaling/main.tf
@@ -18,7 +18,9 @@ resource "aws_appautoscaling_policy" "service_cpu" {
   service_namespace  = aws_appautoscaling_target.service.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value = 50
+    target_value       = 50
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
 
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
@@ -33,6 +35,8 @@ resource "aws_iam_policy" "autoscale" {
 }
 
 data "aws_iam_policy_document" "autoscale" {
+  # checkov:skip=CKV_AWS_109 Allow autoscaling to work
+  # @TODO narrow resource list for autoscaling permissions
   statement {
     sid    = "AllowAutoScaling"
     effect = "Allow"
