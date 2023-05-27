f57131e94cd849541bac3a52b41f35fb4b914580
Rocket ! <rocketnova@users.noreply.github.com>
2023-05-26 23:45:09 -0400

    Add checkov skips.

--- infra/modules/s3-logging/main.tf
+++ infra/modules/s3-logging/main.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket" "s3_encrypted_log" {
   # checkov:skip=CKV_AWS_144:Cross region replication not required by default
   # checkov:skip=CKV2_AWS_61:Lifecycle policy will be added in later ticket for post-pilot cleanup
   # checkov:skip=CKV2_AWS_62:Disable SNS requirement
+  # checkov:skip=CKV_AWS_145:Access logging for s3 and elb does not support KMS SSE encryption
 }
 
 resource "aws_s3_bucket_versioning" "s3_encrypted_log" {
